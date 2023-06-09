7bc70d0c9c9be5826ba0888d5d5d846d5f56acf7
Pratik Kontamwar <pratikkontamwar25@gmail.com>
2023-06-10 01:05:40 +0530

    add login and register component

--- book-shopping-app/src/app/app.module.ts
+++ book-shopping-app/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +14,8 @@ import { CartComponent } from './cart/cart.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BooksModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
